9bf0f8585ae5a48700e79c66a62eaff357cf398b
Nipun Agarwal <111996206+Lucirace@users.noreply.github.com>
2024-06-30 19:45:24 +0530

    Update Appbar.tsx

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -11,12 +11,10 @@ interface AppbarProps {
 
 export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
   return (
-    <div className="flex justify-between border-b px-4 border-slate-900 dark:bg-gray-900">
-      <img src="logo2.svg" className="w-10 h-11 pt-1 " />
-      <div className="text-lg flex flex-col justify-center font-semibold whitespace-nowrap dark:text-white">
-        BHARAT WALLET
-      </div>
-      <div className="flex flex-col justify-center pt-2">
+    <div className="flex justify-between border-b-4 px-4 border-slate-900 dark:bg-gray-900">
+      <img src="logo3.svg" className="w-10 h-11 pt-3 " />
+
+      <div className="flex flex-col justify-center pt-3">
         <Button onClick={user ? onSignout : onSignin}>
           {user ? "Logout" : "Login"}
         </Button>
